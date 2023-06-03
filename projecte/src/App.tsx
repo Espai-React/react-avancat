cc445e9e4d0f75e9cd00583207393b4e96e5211a
llopFilms <llopfilms@gmail.com>
2023-06-03 11:53:30 +0200

    3. Codi declaratiu . JSX . Botó comptador

--- projecte/src/App.tsx
+++ projecte/src/App.tsx
@@ -1,5 +1,23 @@
-function App() {
-  return <div>Hola al món!</div>
+import { useState } from "react";
+
+
+const App = () => {
+
+  const [comptador, setComptador] = useState(0);
+  console.log("Comptador: ", comptador);
+
+  const handleClick = () => {
+    setComptador(comptador + 1);    
+  }
+
+  return (
+    <div>
+      <input type="button" value={`Clica! ${comptador}`} onClick={handleClick}/>
+      <p>Comptador: {comptador}</p>
+    </div>
+  )
+  
 }
 
-export default App
+export default App;
+
