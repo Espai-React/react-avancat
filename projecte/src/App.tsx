651ad94e322ac21c918d17212f760013d5c4f558
llopFilms <llopfilms@gmail.com>
2023-06-03 13:25:09 +0200

    Actualització

--- projecte/src/App.tsx
+++ projecte/src/App.tsx
@@ -1,23 +1,33 @@
-import { useState } from "react";
-
+import { useEffect, useRef, useState } from 'react'
 
 const App = () => {
+  const [comptador, setComptador] = useState<number>(0)
+  const data = useRef<string | null>(null)
+  console.log('Comptador: ', comptador)
 
-  const [comptador, setComptador] = useState(0);
-  console.log("Comptador: ", comptador);
-
-  const handleClick = () => {
-    setComptador(comptador + 1);    
+  const handleClick = (): void => {
+    setComptador(comptador + 1)
   }
+
+  useEffect(() => {
+    data.current = new Date().toLocaleTimeString()
+  }, [comptador])
 
   return (
     <div>
-      <input type="button" value={`Clica! ${comptador}`} onClick={handleClick}/>
+      {!comptador ? (
+        <h3>Bon dia. Esperant comptador</h3>
+      ) : (
+        <h3>Comptador a les {data.current}</h3>
+      )}
+      <input
+        type="button"
+        value={`Clica! ${comptador}`}
+        onClick={handleClick}
+      />
       <p>Comptador: {comptador}</p>
     </div>
   )
-  
 }
 
-export default App;
-
+export default App
