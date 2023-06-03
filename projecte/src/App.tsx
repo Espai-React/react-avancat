90f769800daa96c4c91974da50adba786e5c8e42
llopFilms <llopfilms@gmail.com>
2023-06-03 13:51:06 +0200

    4. Props i estils: . Instal·lació modern-normalize . Importació main.tsx . Preparació projecte a App.tsx

--- projecte/src/App.tsx
+++ projecte/src/App.tsx
@@ -1,33 +1,11 @@
-import { useEffect, useRef, useState } from 'react'
+
 
 const App = () => {
-  const [comptador, setComptador] = useState<number>(0)
-  const data = useRef<string | null>(null)
-  console.log('Comptador: ', comptador)
-
-  const handleClick = (): void => {
-    setComptador(comptador + 1)
-  }
-
-  useEffect(() => {
-    data.current = new Date().toLocaleTimeString()
-  }, [comptador])
-
   return (
     <div>
-      {!comptador ? (
-        <h3>Bon dia. Esperant comptador</h3>
-      ) : (
-        <h3>Comptador a les {data.current}</h3>
-      )}
-      <input
-        type="button"
-        value={`Clica! ${comptador}`}
-        onClick={handleClick}
-      />
-      <p>Comptador: {comptador}</p>
+      <h1>Hello World</h1>
     </div>
   )
 }
 
-export default App
+export default App;
