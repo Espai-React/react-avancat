4de742cf487aefb39dd811c7275c582f90854cec
llopFilms <llopfilms@gmail.com>
2023-06-11 01:14:14 +0200

    Actualització

--- projecte/src/components/organisms/Table/TableBody.tsx
+++ projecte/src/components/organisms/Table/TableBody.tsx
@@ -4,15 +4,20 @@ import type { TTable } from './types';
 
 type TTableBody = TTable;
 
-const TableBody: FC<TTableBody> = ({ columns, data }) =>
-	<tbody>
-		{data.map((row) => (
-			<tr key={row.id}>
-				{columns.map((column) => (
-					<td key={column.id}>{row[column.id]}</td>
-				))}
-			</tr>
-		))}
-	</tbody>;
+const TableBody: FC<TTableBody> = ({ columns, data }) => (
+  <tbody>
+    {data.map((row) => {
+      // console.log('row', row);
+      return (
+        <tr key={row.id}>
+					{columns.map((column) => {
+						// console.log('column.id', column.id, "dada", row[column.id]);
+            return <td key={column.id}>{row[column.id]}</td>;
+          })}
+        </tr>
+      );
+    })}
+  </tbody>
+);
 
 export default styled(TableBody)``;
